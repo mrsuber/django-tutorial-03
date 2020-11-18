431180a4f76efb0be6da1a733f24b394220ae387
Your Name <you@example.com>
2020-11-18 17:11:58 +0100

    updates on the mysite

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 app_name = 'shop'
 
 urlpatterns = [
+    
     url(r'^$', views.product_list, name='product_list'),
     url(r'^(?P<category_slug>[-\w]+)/$',
         views.product_list, name='product_list_by_category'),
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,20 +1,49 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+
+
 
 
 def product_list(request, category_slug=None):
 	category = None
 	categories = Category.objects.all()
 	products = Product.objects.filter(available=True)
+
+	page = request.GET.get('page', 1)
+	paginator = Paginator(products, 8)
+	try:
+		product_list = paginator.page(page)
+	except PageNotAnInteger:
+		product_list = paginator.page(1)
+	except EmptyPage:
+		product_list = paginator.page(paginator.num_pages)
+
+
+
+
+
+
 	if category_slug:
 		category = get_object_or_404(Category, slug=category_slug)
 		products = Product.objects.filter(category=category)
 
+		page = request.GET.get('page', 1)
+		paginator = Paginator(products, 8)
+		try:
+			product_list = paginator.page(page)
+		except PageNotAnInteger:
+			product_list = paginator.page(1)
+		except EmptyPage:
+			product_list = paginator.page(paginator.num_pages)
+
 	context = {
 		'category': category,
 		'categories': categories,
-		'products': products
+		'product_list': product_list
 	}
 	return render(request, 'shop/product/list.html', context)
 
